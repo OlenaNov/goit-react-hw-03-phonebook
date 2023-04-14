04898198a3af09c6514a043d9d01fbf20c70ccb1
Olena Novitska <109989618+OlenaNov@users.noreply.github.com>
2023-04-14 09:36:37 +0200

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,16 +6,26 @@ import ContactForm from "./ContactForm";
 import ContactList from "./ContactList";
 import Filter from "./Filter";
 
+const CONTACTS_STORAGE_KEY = 'contacts-storage';
+
 export class App extends Component {
 
   state = {
-    contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
+    contacts: [],
     filter: '',
+  };
+
+  componentDidMount() {
+    const contactsData = JSON.parse(localStorage.getItem(CONTACTS_STORAGE_KEY));
+    if(contactsData) {
+      this.setState({ contacts: contactsData });
+    };
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.contacts !== this.state.contacts) {
+      localStorage.setItem(CONTACTS_STORAGE_KEY, JSON.stringify(this.state.contacts));
+    }
   };
             
   addContact = (name, number) => {
